390c84555c4c8f897d38881785bfda258184c61f
Xanuthatusu <anthony@tepia.net>
2018-04-27 15:38:03 -0600

    Update keymap

--- keyboards/planck/keymaps/xanuthatusu/keymap.c
+++ keyboards/planck/keymaps/xanuthatusu/keymap.c
@@ -25,7 +25,8 @@ enum planck_layers {
   RAISE_LAYER,
   LOWER_LAYER,
   ADJUST_LAYER,
-  GUI_LAYER
+  GUI_LAYER,
+  FUNC_LAYER
 };
 
 enum planck_keycodes {
@@ -33,27 +34,32 @@ enum planck_keycodes {
   QWERTY,
   LOWER,
   RAISE,
-  GUI_KEY
+  GUI_KEY,
+  FUNC_KEY
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Colemak
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |Space |Space |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
+ *                ,-----------------------------------------------------------------------------------.
+ *                | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Func |
+ *                |------+------+------+------+------+-------------+------+------+------+------+------|
+ * Tap for Esc -- | Ctrl |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
+ *                |------+------+------+------+------+------|------+------+------+------+------+------|
+ *   Tap for ( -- |LShift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |RShift|
+ *                |------+------+------+------+------+------+------+------+------+------+------+------|
+ *                |  Mod |      | Caps |  Alt |Lower | Bksp |  GUI |Raise |  Mod |      |      |Enter |
+ *                `-----------------------------------------------------------------------------------'
+ *                                                              ^
+ *                                                              |
+ * Tap for Space -----------------------------------------------'
  */
+
 [COLEMAK_LAYER] = {
-  {KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,  KC_G,    KC_J,                  KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_NO  },
-  {CTL_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,  KC_D,    KC_H,                  KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT},
-  {KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_K,                  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSPC},
-  {KC_LGUI,        KC_SLEP, KC_CAPS, KC_LALT, LOWER, KC_BSPC, LT(GUI_LAYER, KC_SPC), RAISE, KC_LGUI, KC_NO,  KC_NO,   KC_ENT }
+  {KC_TAB,         KC_Q,  KC_W,    KC_F,    KC_P,  KC_G,    KC_J,                  KC_L,  KC_U,    KC_Y,   KC_SCLN, FUNC_KEY},
+  {CTL_T(KC_ESC),  KC_A,  KC_R,    KC_S,    KC_T,  KC_D,    KC_H,                  KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT },
+  {KC_LSPO,        KC_Z,  KC_X,    KC_C,    KC_V,  KC_B,    KC_K,                  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSPC },
+  {KC_LGUI,        KC_NO, KC_CAPS, KC_LALT, LOWER, KC_BSPC, LT(GUI_LAYER, KC_SPC), RAISE, KC_LGUI, KC_NO,  KC_NO,   KC_ENT  }
 },
 
 /* Qwerty
@@ -81,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Sup-`|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |   `  |   -  |   =  |   [  |   ]  |  \   |
+ * |      |      |      |      |      |      |   `  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |   _  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -89,10 +95,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [RAISE_LAYER] = {
-  {LGUI(KC_GRV), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-  {KC_DEL,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
+  {LGUI(KC_GRV), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  {_______,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
   {_______,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______},
-  {_______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______,      _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______}
 },
 
 /* Lower
@@ -107,10 +113,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [LOWER_LAYER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_BSPC},
-  {KC_DEL,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, _______, _______, _______, KC_PGUP, KC_PGDN, KC_PIPE},
+  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______},
+  {_______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, _______, _______, _______, KC_PGUP, KC_PGDN, KC_PIPE},
   {_______, _______, _______, _______, KC_OPER, KC_MUTE, _______, _______, _______, KC_HOME, KC_END,  _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______}
 },
 
 /* Adjust (Lower + Raise)
@@ -147,6 +153,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO},
   {KC_NO, KC_NO, KC_NO,   KC_BTN3, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO},
   {KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   _______, _______, KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO}
+},
+
+/* Function
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |  F13 |  F14 |  F15 |  F16 |  F17 |  F18 |  F19 |  F20 |  F21 |  F22 |  F23 |  F24 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[FUNC_LAYER] = {
+  {KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  _______},
+  {KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12 },
+  {KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24 },
+  {KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  }
 }
 
 };
@@ -193,6 +217,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case FUNC_KEY:
+      if (record->event.pressed) {
+        layer_on(FUNC_LAYER);
+      } else {
+        layer_off(FUNC_LAYER);
+      }
     case COLEMAK:
       if (record->event.pressed) {
         set_single_persistent_default_layer(COLEMAK_LAYER);
