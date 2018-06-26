899db7bff05789cd9b596dbda684edbb440744a0
Xanuthatusu <anthony@tepia.net>
2018-06-26 12:24:10 -0600

    Add arrow key changes to GUI layer

--- keyboards/planck/keymaps/xanuthatusu/keymap.c
+++ keyboards/planck/keymaps/xanuthatusu/keymap.c
@@ -149,10 +149,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [GUI_LAYER] = {
-  {KC_NO, KC_NO, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO},
-  {KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO},
-  {KC_NO, KC_NO, KC_NO,   KC_BTN3, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO},
-  {KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   _______, _______, KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO}
+  {KC_NO, KC_NO, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO},
+  {KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_LEFT, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO},
+  {KC_NO, KC_NO, KC_NO,   KC_BTN3, KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO},
+  {KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   _______, _______, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO}
 },
 
 /* Function
