bfc5a857c396f152e71587102cf4b5cc0681fa07
Anthony George <anthony@tepia.net>
2019-01-31 17:07:33 -0700

    Update Qwerty

--- keyboards/lets_split/keymaps/xanuthatusu/keymap.c
+++ keyboards/lets_split/keymaps/xanuthatusu/keymap.c
@@ -58,10 +58,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [QWERTY_LAYER] = LAYOUT_ortho_4x12(
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-   KC_LGUI,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,                   KC_U,    KC_I,    KC_O,    KC_P,    LT(FUNC_LAYER, KC_DEL), \
+   CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,                   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+   KC_LSPO,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,                   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC , \
+   KC_LGUI,        KC_NO,   KC_CAPS, KC_LALT, LOWER,   KC_BSPC, LT(GUI_LAYER, KC_SPC),  RAISE,   KC_LALT, KC_NO,   KC_NO,   KC_ENT  \
 ),
 
 /* Lower
